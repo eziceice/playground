caf61d2eec40dbc7ff3dc939b7bf16422ad9ebee
eziceice <liyutian6@gmail.com>
2017-04-28 19:05:32 +1000

    ReverseString

---
+++ src/ReverseString.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Ryan on 2017/4/28.
+ */
+public class ReverseString {
+    public static String reverseString(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        char[] result = s.toCharArray();
+        while (left < right) {
+            char temp = result[left];
+            result[left] = result[right];
+            result[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(result);
+    }
+
+    public static String reverseStringStringBuilder(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("hello"));
+    }
+}
