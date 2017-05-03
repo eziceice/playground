3d0f556c0fe6981f4252833314cd600b06d827fe
eziceice <liyutian6@gmail.com>
2017-05-03 15:24:53 +1000

    Delete node in a single linked list

---
+++ src/DeleteNodeInLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Ryan on 2017/5/3.
+ */
+public class DeleteNodeInLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 因为无法链接到该node的前结点，可以将该node的next复制到该node然后将next删除
+     *
+     */
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
